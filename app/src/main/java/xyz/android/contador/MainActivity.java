89e3eabf1c6686d3d2aff9c55b8b4cef8e4cd497
Huansha13 <ing.yfhl13@outlook.com>
2023-03-18 19:54:51 -0500

    ejemplo con libreria Material

---
+++ app/src/main/java/xyz/android/contador/MainActivity.java
@@ -0,0 +1,81 @@
+package xyz.android.contador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private TextView mTextView;
+    private int mCounter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.text_view);
+
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", mCounter);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mCounter = savedInstanceState.getInt("counter");
+        mTextView.setText("Counter: " + mCounter);
+        Log.d(TAG, "onRestoreInstanceState");
+    }
+
+    public void incrementCounter(View view) {
+        mCounter++;
+        mTextView.setText("Counter: " + mCounter);
+    }
+
+    public void resetCounter(View view) {
+        mCounter = 0;
+        mTextView.setText("Counter: " + mCounter);
+    }
+}
